8f9b798a2e3fd48a1b12899a42880ed959a67643
John Catchpole <johncatchpole2@gmail.com>
2022-10-25 15:25:33 +0100

    Creating function to pass test cases on repeatString.js and Editing repeatString.spec.js to enable all test cases for npm testing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+let concatedString = '';
 
+  if(num < 0)
+  {
+    return 'ERROR';
+  }  
+    for(let i = 0; i < num; i++)
+    {        
+        //Add the string input to the variable to hold the result
+        concatedString += string
+    }
+    return concatedString;
 };
 
 // Do not edit below this line
