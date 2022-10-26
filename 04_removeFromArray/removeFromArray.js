5cea6a499ed235f6d1088acf9ccb170fa3074e2d
John Catchpole <johncatchpole2@gmail.com>
2022-10-26 11:43:24 +0100

    Creating function to pass test condition in removeFromArray.js and enabling all test cases in removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    //the very first item  in our list of arguments is the array,
+    //we will pull it out with args[0]
+    const array = args[0];
+    //create a new empty array
+    const newArray = [];
+    
+    array.forEach(element => {
+        //push every element into the new array
+        //UNLESS it is included in the function arguments
+        //so we create a new array with every item, except those that should be removed
+        if(!args.includes(element)){ //if args doesn't include element
+            newArray.push(element) //push(add)it to the new array
+        }
+    });
+    //and return that array
+    return newArray;
 };
 
 // Do not edit below this line
