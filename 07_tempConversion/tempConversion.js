f497a01897b46830412d96a6fb983c4d2ee4fa15
John Catchpole <johncatchpole2@gmail.com>
2022-10-31 15:11:23 +0000

    Creating the function to convert inputs from one temp type to another in tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
-
+//Farenheit to celsius
+const ftoc = function(temp) {
+  //Conversion from Cel to Farenheit(temp - 32 * 5/9 = result)
+  let conv = (temp - 32) * 5/9;
+  //Round number to one decimal place
+  let roundedResult = Math.round(conv * 10) / 10;
+  return roundedResult;
 };
-
-const ctof = function() {
-
+//Celsius to farneheit
+const ctof = function(temp) {
+//Conversion Forumla Cel to Faren(temp * 9/5 + 32)
+let conversion = (temp * 9/5) + 32;
+//Round number to one decimal place
+let result = Math.round(conversion * 10) / 10;
+return result;
 };
 
 // Do not edit below this line
