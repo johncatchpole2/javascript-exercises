c2e4be6b4d7dc18bd34568bd5b424ed5c48d3b5a
John Catchpole <johncatchpole2@gmail.com>
2022-10-31 12:41:27 +0000

    Creating the function in sumAll.js and Editing spec code to enable all testing scenarios in sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    //Create a variable to hold the final sum
+    let finalSum = 0;
+    //loop through the given numbers
+    for(let i = 0; i<= lastNum; i++){
+        //On each iteration add the number to the sum
+        finalSum += i;
+    }
+    //return the sum after finishing the loop
+    return finalSum;
 };
 
 // Do not edit below this line
