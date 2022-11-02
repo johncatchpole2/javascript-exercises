b2a412c2820e0ce08ce5cea67d2d3ccab65b2294
John Catchpole <johncatchpole2@gmail.com>
2022-11-02 12:04:22 +0000

    Adding code so when executed all functions perform as needed to pass the test conditions in calculator.js & Editing calculator.spec.js to enable all test conditions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(para1,para2) {
+
+	if(para1 === 0 || para2 ===0){
+    return para1 + para2;
+  }
+  let result = para1 + para2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(para1,para2) {
+  let result = para1-para2;
+	return result;
 };
 
-const sum = function() {
-	
+const sum = function(array = []) {
+return array.reduce((total,current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array = []) {
+  return array.length //ternary Conditional
+  ? array.reduce((accumlator, nextItem) => accumlator * nextItem)
+  : 0 ;
 };
 
-const power = function() {
-	
+const power = function(num,powerOf) {
+	return Math.pow(num,powerOf);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  //Recursive Factorial
+	if(num === 0)
+  {
+    return 1;
+  }
+  return num * factorial (num-1);
 };
 
 // Do not edit below this line
