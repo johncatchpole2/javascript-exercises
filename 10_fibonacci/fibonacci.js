bd22e8adc41bee2fbc40232b7ce23158944b4709
John Catchpole <johncatchpole2@gmail.com>
2022-11-03 15:30:43 +0000

    Creating for loop in function to Find Fibonacci sequence in fibonacci.js & Enabling all test conditions(removing .skip) to ensure function passes all criteria in fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fibNum) {
+  let a = 0;
+  let b = 1;    
+    
+      if(fibNum < 0){
+            return 'OOPS';
+        }
+      if(fibNum === 0){
+            return 0;
+        }
 
-};
+      for(let i = 1;i < fibNum; i++){
+        const reg = b; //1
+        b = a + b;//First ite: 0+1 = 1
+        a = reg;// 1
+      }
+      return b;
+}; 
 
 // Do not edit below this line
 module.exports = fibonacci;
